6d0ce9181a99b626f4d29e08d24ded71d2283320
Falcion <57592842+Falcion@users.noreply.github.com>
2020-12-20 15:55:18 +0300

    Cleaning up the repository

--- Context/Template/KubeJS/data/transcendia/kubejs/tags/minecraft/leaves.js
+++
@@ -1,6 +0,0 @@
-events.listen('item.tags', (event) => {
-
-    event.get('minecraft:leaves').remove('minecraft:brown_mushroom_block')
-    event.get('minecraft:leaves').remove('minecraft:red_mushroom_block')
-
-  })
